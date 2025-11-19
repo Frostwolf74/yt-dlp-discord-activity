2b2ce1de2600ee677ad3982b7d2c5ae23061c0bc
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-11-19 11:35:18 -0700

    added explicit declaration of routes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ import cors from "cors";
 import fs from "fs";
 import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/download", (req, res) => {
     const name = req.body.name;
